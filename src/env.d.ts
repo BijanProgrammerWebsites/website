332aff2565bbc9c5dd3471dd74bce865495feaca
BijanProgrammer <bijaneisapour@gmail.com>
2025-04-15 10:49:06 +0330

    [ADD] phone and email conditionally

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PHONE_NUMBER: string;
+    readonly PHONE_NUMBER_TITLE: string;
+    readonly EMAIL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
